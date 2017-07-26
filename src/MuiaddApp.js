b85f83a971ba609f73e6a2a3990d9299194a5e86
meteorcyclops <meteorcyclops1104@gmail.com>
2017-07-26 15:12:30 +0800

    add mui theme.

--- src/MuiaddApp.js
+++ src/MuiaddApp.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
 import {observer} from 'mobx-react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import prepareTheme from './prepare_theme'
 import App from './App'
 import mobx from 'mobx'
 import _ from 'lodash'
+
 
 class MuiaddApp extends React.Component {
     constructor(props) {
@@ -18,7 +20,7 @@ class MuiaddApp extends React.Component {
 
     render() {
         return (
-            <MuiThemeProvider>
+            <MuiThemeProvider muiTheme={prepareTheme}>
                 <App/>
             </MuiThemeProvider>
         );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiaddApp from './MuiaddApp'
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './index.css';
+
+
+try{
+    injectTapEventPlugin()
+}catch(err){
+    console.log('使用injectTapEventPlugin出現錯誤(index.js), 錯誤資訊：', err)
+}
 
 ReactDOM.render(
   <MuiaddApp />,
