2432d6ba46adb7fb82891c6304235c010523704d
meteorcyclops <meteorcyclops1104@gmail.com>
2017-08-04 16:59:01 +0800

    keyinfo temp style.

--- src/Route.js
+++ src/Route.js
@@ -26,7 +26,7 @@ class Route extends React.Component {
     render() {
         let transitionClass = "prepare_route_page"
 
-        const page = this.props.page
+        const page = 'info'
         const cancer = this.props.cancer
         const feature = this.props.feature
         const inherit = this.props.inherit
